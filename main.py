621645848f3a9080509643cf357f4af45d6cfd4d
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-06-19 10:35:02 -0400

    add endpoint for generating reports

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # coding=utf-8
+from __future__ import unicode_literals
 import json
 import os.path
 import psycopg2
 import config
+import itertools
+import re
 from collections import namedtuple
+from docutils.core import publish_string
+from datetime import datetime
 
 from bottle import route, run, static_file, hook
 
@@ -322,6 +327,174 @@ def path_per_person():
     return results
 
 
+def manual_test_report():
+    query = """
+    WITH RECURSIVE tree(id, name) AS
+    (
+        SELECT
+            parent.id,
+            CAST(parent.name as varchar(200)) as name
+        FROM
+            nodes_hierarchy parent
+        WHERE
+            parent.parent_id = (
+                SELECT testplans.testproject_id
+                FROM builds
+                INNER JOIN testplans ON builds.testplan_id = testplans.id
+                WHERE builds.id = %(build_id)s
+            )
+        UNION ALL
+        SELECT
+            child.id,
+            CAST(tree.name || '/' || child.name as varchar(200)) as name
+        FROM
+            tree
+            INNER JOIN nodes_hierarchy child
+                ON tree.id = child.parent_id
+                AND child.node_type_id = 2
+    ),
+    latest_executions AS
+    (
+        SELECT
+            executions.tcversion_id AS tcversion_id,
+            MAX(executions.execution_ts) AS execution_ts
+        FROM
+            executions
+        GROUP BY
+            executions.tcversion_id
+    )
+    SELECT
+        tree.name                           AS folder,
+        tcversions.tc_external_id           AS number,
+        parent.name                         AS name,
+        tcversions.version                  AS version,
+        (CASE executions.status
+        WHEN 'p' THEN 'passed'
+        WHEN 'f' THEN 'failed'
+        WHEN 'b' THEN 'blocked'
+        ELSE executions.status
+        END)                                AS status
+    FROM
+        executions
+        INNER JOIN latest_executions
+            ON executions.tcversion_id = latest_executions.tcversion_id
+            AND executions.execution_ts = latest_executions.execution_ts
+        INNER JOIN builds
+            ON builds.id = executions.build_id
+        INNER JOIN tcversions
+            ON executions.tcversion_id = tcversions.id
+            INNER JOIN nodes_hierarchy node
+                ON tcversions.id = node.id
+                INNER JOIN nodes_hierarchy parent
+                    ON node.parent_id = parent.id
+                    LEFT OUTER JOIN tree
+                        ON parent.parent_id = tree.id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+    ORDER BY
+        tree.name ASC,
+        parent.node_order DESC
+    """
+
+    cursor = connection.cursor()
+    cursor.execute(query, {'build_id': build.id})
+    report = group_executions_by_folder(cursor)
+    cursor.close()
+
+    return report
+
+
+def group_executions_by_folder(cursor):
+    report = []
+    key = lambda row: row[0]
+
+    for folder, rows in itertools.groupby(cursor, key):
+        executions = tuple({'number': row[1],
+                            'name': row[2].decode('utf8'),
+                            'version': row[3],
+                            'status': row[4].decode('utf8')} for row in rows)
+        report.append((folder.decode('utf8'), executions))
+
+    return report
+
+
+def generate_rst(report):
+    lines = []
+
+    title = 'Test report for XiVO {version}'.format(version=build.name)
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = 'Report generated at {date}'.format(date=now)
+
+    lines.extend(_build_title(title, '='))
+    lines.append(timestamp)
+    lines.append('')
+    lines.extend(_build_title('Totals'))
+    lines.extend(_build_totals(report))
+    lines.append('')
+    lines.extend(_build_title('Tests'))
+
+    for folder, executions in report:
+        lines.extend(_build_title(folder, '_'))
+        lines.extend(_build_table(executions))
+        lines.append('')
+
+    markup = '\n'.join(lines)
+    return markup
+
+
+def _build_title(folder, underline='-'):
+    yield _escape_markup(folder)
+    yield underline * len(folder)
+    yield ''
+
+
+def _build_totals(report):
+    totals = {'passed': 0,
+              'blocked': 0,
+              'failed': 0}
+
+    executions = itertools.chain.from_iterable(x[1] for x in report)
+    for execution in executions:
+        status = execution['status']
+        totals[status] += 1
+
+    for name, count in sorted(totals.iteritems(), key=lambda x: x[0]):
+        yield ':{status}: {count}'.format(status=name.capitalize(),
+                                          count=count)
+
+
+def _build_table(executions):
+    table_row = "{name} {status}"
+    name_template = "X-{e[number]}: {e[name]} (v{e[version]})"
+
+    formatted_names = [_escape_markup(name_template.format(e=e)) for e in executions]
+    formatted_statuses = [_escape_markup(e['status'].capitalize()) for e in executions]
+
+    max_name = max(len(n) for n in formatted_names)
+    max_status = max(len(s) for s in formatted_statuses)
+
+    title_row = table_row.format(name='Test'.ljust(max_name),
+                                 status='Status'.ljust(max_status))
+    bar = table_row.format(name='=' * max_name,
+                           status='=' * max_status)
+
+    yield bar
+    yield title_row
+    yield bar
+
+    for name, status in zip(formatted_names, formatted_statuses):
+        yield table_row.format(name=name.ljust(max_name),
+                               status=status.ljust(max_status))
+
+    yield bar
+
+
+def _escape_markup(text):
+    replace = lambda m: '\\' + m.group(1)
+    return re.sub(r'([*`|_])', replace, text)
+
+
 def scoreboard_rows():
     scores = executed_per_person()
     paths = path_per_person()
@@ -362,6 +535,18 @@ def fetch_scoreboard():
     return scoreboard
 
 
+@route('/report.<output>')
+def report_json(output):
+    report = manual_test_report()
+    if output == 'json':
+        return json.dumps(report)
+    elif output == 'rst':
+        return generate_rst(report)
+    markup = generate_rst(report)
+    return publish_string(markup, writer_name=output,
+                          settings_overrides={'input_encoding': 'unicode'})
+
+
 @route('/stats.json')
 def stats_json():
     stats = fetch_stats()
@@ -391,4 +576,4 @@ def fetch_build():
 
 
 if __name__ == "__main__":
-    run(host=config.HOST, port=config.PORT)
+    run(host=config.HOST, port=config.PORT, reloader=config.DEBUG_RELOAD)
