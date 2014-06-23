7dacd87efc7d345d4ea023368157a0b89f1ce9f6
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-06-23 10:41:01 -0400

    move database code into its own module

---
+++ dao.py
@@ -0,0 +1,420 @@
+import config
+import psycopg2
+import itertools
+from collections import namedtuple
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+
+Build = namedtuple('Build', ['id', 'version'])
+build = None
+
+
+class Database(object):
+
+    @classmethod
+    def from_config(cls, cfg):
+        return cls(cfg.DB_HOST,
+                   cfg.DB_PORT,
+                   cfg.DB_NAME,
+                   cfg.DB_USER,
+                   cfg.DB_PASSWORD)
+
+    def __init__(self, host, port, database, user, password):
+        self.connection = psycopg2.connect(host=host,
+                                           port=port,
+                                           database=database,
+                                           user=user,
+                                           password=password)
+
+    def row(self, query, **params):
+        cursor = self._cursor_for_query(query, params)
+        row = cursor.fetchone()
+        cursor.close()
+        return row
+
+    def scalar(self, query, **params):
+        row = self.row(query, **params)
+        return row[0]
+
+    def rows(self, query, **params):
+        cursor = self._cursor_for_query(query, params)
+        for row in cursor:
+            yield row
+        cursor.close()
+
+    def _cursor_for_query(self, query, params):
+        cursor = self.connection.cursor()
+        cursor.execute(query, params)
+        return cursor
+
+
+db = Database.from_config(config)
+
+
+def refresh_build():
+    global build
+    build = latest_build()
+
+
+def latest_build():
+    query = """
+    SELECT
+        builds.id,
+        builds.name
+    FROM
+        builds
+        INNER JOIN testplans
+            ON testplans.id = builds.testplan_id
+            INNER JOIN testprojects
+                ON testprojects.id = testplans.testproject_id
+    WHERE
+        testprojects.notes = %(project)s
+    ORDER BY
+        builds.creation_ts DESC
+    LIMIT 1
+    """
+
+    row = db.row(query, project=config.PROJECT_NAME)
+    return Build(row[0], row[1])
+
+
+def total_manual_tests():
+    query = """
+    SELECT
+        count(tcversions.id)
+    FROM
+        tcversions
+        INNER JOIN testplan_tcversions
+            ON tcversions.id = testplan_tcversions.tcversion_id
+            INNER JOIN builds
+                ON builds.testplan_id = testplan_tcversions.testplan_id
+    WHERE
+        tcversions.execution_type = 1
+        AND builds.id = %(build_id)s
+    GROUP BY
+        builds.id
+    """
+
+    return db.scalar(query, build_id=build.id)
+
+
+def test_statuses():
+    query = """
+    WITH latest_executions AS
+    (
+    SELECT
+        executions.tcversion_id AS tcversion_id,
+        MAX(executions.execution_ts) AS execution_ts
+    FROM
+        executions
+    GROUP BY
+        executions.tcversion_id
+    )
+
+    SELECT
+        (CASE executions.status
+         WHEN 'p' THEN 'passed'
+         WHEN 'f' THEN 'failed'
+         WHEN 'b' THEN 'blocked'
+         ELSE executions.status
+         END)                       AS status,
+        COUNT(executions.status)    AS total
+    FROM
+        executions
+    INNER JOIN latest_executions
+        ON executions.tcversion_id = latest_executions.tcversion_id
+        AND executions.execution_ts = latest_executions.execution_ts
+    INNER JOIN builds
+        ON builds.id = executions.build_id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+    GROUP BY
+        executions.status
+    """
+
+    statuses = {
+        'passed': 0,
+        'failed': 0,
+        'blocked': 0,
+    }
+
+    rows = db.rows(query, build_id=build.id)
+    statuses.update(dict((key, value) for key, value in rows))
+
+    return statuses
+
+
+def tests_for_status(status):
+    query = """
+    WITH latest_executions AS
+    (
+    SELECT
+        executions.tcversion_id         AS tcversion_id,
+        MAX(executions.execution_ts)    AS execution_ts
+    FROM
+        executions
+    GROUP BY
+        executions.tcversion_id
+    )
+
+    SELECT
+        tcversions.tc_external_id       AS number,
+        parent.name                     AS name,
+        executions.notes                AS notes
+    FROM
+        executions
+        INNER JOIN latest_executions
+            ON executions.tcversion_id = latest_executions.tcversion_id
+            AND executions.execution_ts = latest_executions.execution_ts
+        INNER JOIN tcversions
+            ON executions.tcversion_id = tcversions.id
+            INNER JOIN nodes_hierarchy node
+                ON tcversions.id = node.id
+                INNER JOIN nodes_hierarchy parent
+                    ON node.parent_id = parent.id
+        INNER JOIN builds
+            ON builds.id = executions.build_id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+        AND executions.status = %(status)s
+    ORDER BY
+        tcversions.tc_external_id
+    """
+
+    rows = db.rows(query, build_id=build.id, status=status)
+
+    tests = [
+        {'name': "X-%s: %s" % (row[0], row[1]),
+         'notes': row[2].strip()}
+        for row in rows]
+
+    return tests
+
+
+def failed_tests():
+    return tests_for_status('f')
+
+
+def blocked_tests():
+    return tests_for_status('b')
+
+
+def executed_per_person():
+    query = """
+    WITH latest_executions AS
+    (
+    SELECT
+        executions.tcversion_id         AS tcversion_id,
+        MAX(executions.execution_ts)    AS execution_ts
+    FROM
+        executions
+    GROUP BY
+        executions.tcversion_id
+    )
+
+    SELECT
+        users.first || ' ' || users.last    AS name,
+        (CASE executions.status
+         WHEN 'p' THEN 'passed'
+         WHEN 'f' THEN 'failed'
+         WHEN 'b' THEN 'blocked'
+         ELSE executions.status
+         END)                               AS status,
+        COUNT(executions.id)                AS executed
+    FROM
+        executions
+        INNER JOIN latest_executions
+            ON executions.tcversion_id = latest_executions.tcversion_id
+            AND executions.execution_ts = latest_executions.execution_ts
+        INNER JOIN builds
+            ON builds.id = executions.build_id
+        INNER JOIN users
+            ON executions.tester_id = users.id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+    GROUP BY
+        (users.first || ' ' || users.last),
+        executions.status
+    """
+
+    rows = db.rows(query, build_id=build.id)
+
+    scores = {}
+    for row in rows:
+        name, status, executed = row
+        scores.setdefault(name, {})[status] = executed
+
+    result = [{'name': key, 'executed': value}
+              for key, value in scores.iteritems()]
+
+    result.sort(reverse=True, key=lambda x: sum(x['executed'].values()))
+
+    return result
+
+
+def path_for_test(tcversion_id):
+    query = """
+    WITH RECURSIVE test_path(name, id, parent_id) AS
+    (
+        SELECT name, id, parent_id FROM nodes_hierarchy WHERE id = (
+            SELECT
+                parent.parent_id
+            FROM
+                nodes_hierarchy node
+                INNER JOIN nodes_hierarchy parent
+                    ON node.parent_id = parent.id
+            WHERE
+                node.id = %(tcversion_id)s
+        )
+        UNION ALL
+            SELECT
+                child.name,
+                child.id,
+                child.parent_id
+            FROM
+                test_path
+                INNER JOIN nodes_hierarchy child
+                    ON test_path.parent_id = child.id
+                    AND child.node_type_id = 2
+    )
+    SELECT name FROM test_path
+    """
+
+    rows = db.rows(query, tcversion_id=tcversion_id)
+    names = [row[0] for row in rows]
+
+    return " / ".join(reversed(names))
+
+
+def path_per_person():
+    query = """
+    WITH latest_executed AS
+    (
+    SELECT
+        executions.tester_id            AS tester_id,
+        MAX(executions.execution_ts)    AS execution_ts
+    FROM
+        executions
+    GROUP BY
+        executions.tester_id
+    )
+
+    SELECT
+        users.first || ' ' || users.last    AS name,
+        executions.tcversion_id             AS tcversion_id
+    FROM
+        executions
+        INNER JOIN latest_executed
+            ON executions.tester_id = latest_executed.tester_id
+            AND executions.execution_ts = latest_executed.execution_ts
+        INNER JOIN builds
+            ON builds.id = executions.build_id
+        INNER JOIN users
+            ON executions.tester_id = users.id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+    GROUP BY
+        (users.first || ' ' || users.last),
+        executions.tcversion_id
+    """
+
+    rows = db.rows(query, build_id=build.id)
+    results = dict((row[0], path_for_test(row[1])) for row in rows)
+
+    return results
+
+
+def manual_test_report():
+    query = """
+    WITH RECURSIVE tree(id, name) AS
+    (
+        SELECT
+            parent.id,
+            CAST(parent.name as varchar(200)) as name
+        FROM
+            nodes_hierarchy parent
+        WHERE
+            parent.parent_id = (
+                SELECT testplans.testproject_id
+                FROM builds
+                INNER JOIN testplans ON builds.testplan_id = testplans.id
+                WHERE builds.id = %(build_id)s
+            )
+        UNION ALL
+        SELECT
+            child.id,
+            CAST(tree.name || '/' || child.name as varchar(200)) as name
+        FROM
+            tree
+            INNER JOIN nodes_hierarchy child
+                ON tree.id = child.parent_id
+                AND child.node_type_id = 2
+    ),
+    latest_executions AS
+    (
+        SELECT
+            executions.tcversion_id AS tcversion_id,
+            MAX(executions.execution_ts) AS execution_ts
+        FROM
+            executions
+        GROUP BY
+            executions.tcversion_id
+    )
+    SELECT
+        tree.name                           AS folder,
+        tcversions.tc_external_id           AS number,
+        parent.name                         AS name,
+        tcversions.version                  AS version,
+        (CASE executions.status
+        WHEN 'p' THEN 'passed'
+        WHEN 'f' THEN 'failed'
+        WHEN 'b' THEN 'blocked'
+        ELSE executions.status
+        END)                                AS status
+    FROM
+        executions
+        INNER JOIN latest_executions
+            ON executions.tcversion_id = latest_executions.tcversion_id
+            AND executions.execution_ts = latest_executions.execution_ts
+        INNER JOIN builds
+            ON builds.id = executions.build_id
+        INNER JOIN tcversions
+            ON executions.tcversion_id = tcversions.id
+            INNER JOIN nodes_hierarchy node
+                ON tcversions.id = node.id
+                INNER JOIN nodes_hierarchy parent
+                    ON node.parent_id = parent.id
+                    LEFT OUTER JOIN tree
+                        ON parent.parent_id = tree.id
+    WHERE
+        builds.id = %(build_id)s
+        AND executions.execution_type = 1
+    ORDER BY
+        tree.name ASC,
+        parent.node_order DESC
+    """
+
+    rows = db.rows(query, build_id=build.id)
+    report = group_executions_by_folder(rows)
+
+    return report
+
+
+def group_executions_by_folder(rows):
+    report = []
+    key = lambda row: row[0]
+
+    for folder, rows in itertools.groupby(rows, key):
+        executions = tuple({'number': row[1],
+                            'name': row[2],
+                            'version': row[3],
+                            'status': row[4]} for row in rows)
+        report.append((folder, executions))
+
+    return report
--- main.py
+++ main.py
@@ -2,430 +2,24 @@
 from __future__ import unicode_literals
 import json
 import os.path
-import psycopg2
 import config
-import itertools
 import re
-from collections import namedtuple
+import itertools
 from docutils.core import publish_string
 from datetime import datetime
+
+import dao
 
 from bottle import route, run, static_file, hook
 
 STATIC_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(__file__), "static"))
 
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
-connection = psycopg2.connect(host=config.DB_HOST,
-                              port=config.DB_PORT,
-                              database=config.DB_NAME,
-                              user=config.DB_USER,
-                              password=config.DB_PASSWORD)
-
-
-Build = namedtuple('Build', ['id', 'name'])
-
-build = None
-
-
-def latest_build():
-    query = """
-    SELECT
-        builds.id,
-        builds.name
-    FROM
-        builds
-        INNER JOIN testplans
-            ON testplans.id = builds.testplan_id
-            INNER JOIN testprojects
-                ON testprojects.id = testplans.testproject_id
-    WHERE
-        testprojects.notes = %(project)s
-    ORDER BY
-        builds.creation_ts DESC
-    LIMIT 1
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'project': config.PROJECT_NAME})
-
-    row = cursor.fetchone()
-    cursor.close()
-
-    return Build(row[0], row[1])
-
-
-def total_manual_tests():
-    query = """
-    SELECT
-        count(tcversions.id)
-    FROM
-        tcversions
-        INNER JOIN testplan_tcversions
-            ON tcversions.id = testplan_tcversions.tcversion_id
-            INNER JOIN builds
-                ON builds.testplan_id = testplan_tcversions.testplan_id
-    WHERE
-        tcversions.execution_type = 1
-        AND builds.id = %(build_id)s
-    GROUP BY
-        builds.id
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id})
-    return cursor.fetchone()[0]
-
-
-def test_statuses():
-    query = """
-    WITH latest_executions AS
-    (
-    SELECT
-        executions.tcversion_id AS tcversion_id,
-        MAX(executions.execution_ts) AS execution_ts
-    FROM
-        executions
-    GROUP BY
-        executions.tcversion_id
-    )
-
-    SELECT
-        (CASE executions.status
-         WHEN 'p' THEN 'passed'
-         WHEN 'f' THEN 'failed'
-         WHEN 'b' THEN 'blocked'
-         ELSE executions.status
-         END)                       AS status,
-        COUNT(executions.status)    AS total
-    FROM
-        executions
-    INNER JOIN latest_executions
-        ON executions.tcversion_id = latest_executions.tcversion_id
-        AND executions.execution_ts = latest_executions.execution_ts
-    INNER JOIN builds
-        ON builds.id = executions.build_id
-    WHERE
-        builds.id = %(build_id)s
-        AND executions.execution_type = 1
-    GROUP BY
-        executions.status
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id})
-
-    statuses = {
-        'passed': 0,
-        'failed': 0,
-        'blocked': 0,
-    }
-
-    statuses.update(dict((key, value) for key, value in cursor))
-    cursor.close()
-
-    return statuses
-
-
-def statuses_and_total_executed():
-    sums = test_statuses()
-    total_executed = sum(sums.values())
-    return sums, total_executed
-
-
-def tests_for_status(status):
-    query = """
-    WITH latest_executions AS
-    (
-    SELECT
-        executions.tcversion_id         AS tcversion_id,
-        MAX(executions.execution_ts)    AS execution_ts
-    FROM
-        executions
-    GROUP BY
-        executions.tcversion_id
-    )
-
-    SELECT
-        tcversions.tc_external_id       AS number,
-        parent.name                     AS name,
-        executions.notes                AS notes
-    FROM
-        executions
-        INNER JOIN latest_executions
-            ON executions.tcversion_id = latest_executions.tcversion_id
-            AND executions.execution_ts = latest_executions.execution_ts
-        INNER JOIN tcversions
-            ON executions.tcversion_id = tcversions.id
-            INNER JOIN nodes_hierarchy node
-                ON tcversions.id = node.id
-                INNER JOIN nodes_hierarchy parent
-                    ON node.parent_id = parent.id
-        INNER JOIN builds
-            ON builds.id = executions.build_id
-    WHERE
-        builds.id = %(build_id)s
-        AND executions.execution_type = 1
-        AND executions.status = %(status)s
-    ORDER BY
-        tcversions.tc_external_id
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id, 'status': status})
-
-    tests = [{
-        'name': "X-%s: %s" % (row[0], row[1]),
-        'notes': row[2].strip(),
-    } for row in cursor]
-
-    cursor.close()
-
-    return tests
-
-
-def failed_tests():
-    return tests_for_status('f')
-
-
-def blocked_tests():
-    return tests_for_status('b')
-
-
-def executed_per_person():
-    query = """
-    WITH latest_executions AS
-    (
-    SELECT
-        executions.tcversion_id         AS tcversion_id,
-        MAX(executions.execution_ts)    AS execution_ts
-    FROM
-        executions
-    GROUP BY
-        executions.tcversion_id
-    )
-
-    SELECT
-        users.first || ' ' || users.last    AS name,
-        (CASE executions.status
-         WHEN 'p' THEN 'passed'
-         WHEN 'f' THEN 'failed'
-         WHEN 'b' THEN 'blocked'
-         ELSE executions.status
-         END)                               AS status,
-        COUNT(executions.id)                AS executed
-    FROM
-        executions
-        INNER JOIN latest_executions
-            ON executions.tcversion_id = latest_executions.tcversion_id
-            AND executions.execution_ts = latest_executions.execution_ts
-        INNER JOIN builds
-            ON builds.id = executions.build_id
-        INNER JOIN users
-            ON executions.tester_id = users.id
-    WHERE
-        builds.id = %(build_id)s
-        AND executions.execution_type = 1
-    GROUP BY
-        (users.first || ' ' || users.last),
-        executions.status
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id})
-
-    scores = {}
-    for row in cursor:
-        name, status, executed = row
-        scores.setdefault(name, {})[status] = executed
-
-    result = [{'name': key, 'executed': value}
-              for key, value in scores.iteritems()]
-
-    result.sort(reverse=True, key=lambda x: sum(x['executed'].values()))
-
-    cursor.close()
-
-    return result
-
-
-def path_for_test(tcversion_id):
-    query = """
-    WITH RECURSIVE test_path(name, id, parent_id) AS
-    (
-        SELECT name, id, parent_id FROM nodes_hierarchy WHERE id = (
-            SELECT
-                parent.parent_id
-            FROM
-                nodes_hierarchy node
-                INNER JOIN nodes_hierarchy parent
-                    ON node.parent_id = parent.id
-            WHERE
-                node.id = %(tcversion_id)s
-        )
-        UNION ALL
-            SELECT
-                child.name,
-                child.id,
-                child.parent_id
-            FROM
-                test_path
-                INNER JOIN nodes_hierarchy child
-                    ON test_path.parent_id = child.id
-                    AND child.node_type_id = 2
-    )
-    SELECT name FROM test_path
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'tcversion_id': tcversion_id})
-
-    names = [row[0] for row in cursor]
-    cursor.close()
-
-    return " / ".join(reversed(names))
-
-
-def path_per_person():
-    query = """
-    WITH latest_executed AS
-    (
-    SELECT
-        executions.tester_id            AS tester_id,
-        MAX(executions.execution_ts)    AS execution_ts
-    FROM
-        executions
-    GROUP BY
-        executions.tester_id
-    )
-
-    SELECT
-        users.first || ' ' || users.last    AS name,
-        executions.tcversion_id             AS tcversion_id
-    FROM
-        executions
-        INNER JOIN latest_executed
-            ON executions.tester_id = latest_executed.tester_id
-            AND executions.execution_ts = latest_executed.execution_ts
-        INNER JOIN builds
-            ON builds.id = executions.build_id
-        INNER JOIN users
-            ON executions.tester_id = users.id
-    WHERE
-        builds.id = %(build_id)s
-        AND executions.execution_type = 1
-    GROUP BY
-        (users.first || ' ' || users.last),
-        executions.tcversion_id
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id})
-
-    results = dict((row[0], path_for_test(row[1])) for row in cursor)
-    cursor.close()
-
-    return results
-
-
-def manual_test_report():
-    query = """
-    WITH RECURSIVE tree(id, name) AS
-    (
-        SELECT
-            parent.id,
-            CAST(parent.name as varchar(200)) as name
-        FROM
-            nodes_hierarchy parent
-        WHERE
-            parent.parent_id = (
-                SELECT testplans.testproject_id
-                FROM builds
-                INNER JOIN testplans ON builds.testplan_id = testplans.id
-                WHERE builds.id = %(build_id)s
-            )
-        UNION ALL
-        SELECT
-            child.id,
-            CAST(tree.name || '/' || child.name as varchar(200)) as name
-        FROM
-            tree
-            INNER JOIN nodes_hierarchy child
-                ON tree.id = child.parent_id
-                AND child.node_type_id = 2
-    ),
-    latest_executions AS
-    (
-        SELECT
-            executions.tcversion_id AS tcversion_id,
-            MAX(executions.execution_ts) AS execution_ts
-        FROM
-            executions
-        GROUP BY
-            executions.tcversion_id
-    )
-    SELECT
-        tree.name                           AS folder,
-        tcversions.tc_external_id           AS number,
-        parent.name                         AS name,
-        tcversions.version                  AS version,
-        (CASE executions.status
-        WHEN 'p' THEN 'passed'
-        WHEN 'f' THEN 'failed'
-        WHEN 'b' THEN 'blocked'
-        ELSE executions.status
-        END)                                AS status
-    FROM
-        executions
-        INNER JOIN latest_executions
-            ON executions.tcversion_id = latest_executions.tcversion_id
-            AND executions.execution_ts = latest_executions.execution_ts
-        INNER JOIN builds
-            ON builds.id = executions.build_id
-        INNER JOIN tcversions
-            ON executions.tcversion_id = tcversions.id
-            INNER JOIN nodes_hierarchy node
-                ON tcversions.id = node.id
-                INNER JOIN nodes_hierarchy parent
-                    ON node.parent_id = parent.id
-                    LEFT OUTER JOIN tree
-                        ON parent.parent_id = tree.id
-    WHERE
-        builds.id = %(build_id)s
-        AND executions.execution_type = 1
-    ORDER BY
-        tree.name ASC,
-        parent.node_order DESC
-    """
-
-    cursor = connection.cursor()
-    cursor.execute(query, {'build_id': build.id})
-    report = group_executions_by_folder(cursor)
-    cursor.close()
-
-    return report
-
-
-def group_executions_by_folder(cursor):
-    report = []
-    key = lambda row: row[0]
-
-    for folder, rows in itertools.groupby(cursor, key):
-        executions = tuple({'number': row[1],
-                            'name': row[2],
-                            'version': row[3],
-                            'status': row[4]} for row in rows)
-        report.append((folder, executions))
-
-    return report
-
-
-def generate_rst(report):
+def generate_rst(report, version):
     lines = []
 
-    title = 'Test report for XiVO {version}'.format(version=build.name)
+    title = 'Test report for XiVO {version}'.format(version=version)
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     timestamp = 'Report generated at {date}'.format(date=now)
 
@@ -499,8 +93,8 @@ def _escape_markup(text):
 
 
 def scoreboard_rows():
-    scores = executed_per_person()
-    paths = path_per_person()
+    scores = dao.executed_per_person()
+    paths = dao.path_per_person()
 
     for person in scores:
         person['last_path'] = paths[person['name']]
@@ -508,44 +102,49 @@ def scoreboard_rows():
     return scores
 
 
+def statuses_and_total_executed():
+    sums = dao.test_statuses()
+    total_executed = sum(sums.values())
+    return sums, total_executed
+
+
 def fetch_stats():
     statuses, total_executed = statuses_and_total_executed()
     stats = {
-        'total_manual': total_manual_tests(),
+        'total_manual': dao.total_manual_tests(),
         'total_executed': total_executed,
         'statuses': statuses,
         'lists': {
-            'failed': failed_tests(),
-            'blocked': blocked_tests(),
+            'failed': dao.failed_tests(),
+            'blocked': dao.blocked_tests(),
         },
-        'version': build.name
+        'version': dao.build.version
     }
-    connection.commit()
 
     return stats
 
 
 def fetch_scoreboard():
-    total = total_manual_tests()
+    total = dao.total_manual_tests()
 
     scoreboard = {
         'total': total,
         'rows': scoreboard_rows(),
-        'version': build.name
+        'version': dao.build.version
     }
-    connection.commit()
 
     return scoreboard
 
 
 @route('/report.<output>')
 def report_json(output):
-    report = manual_test_report()
+    report = dao.manual_test_report()
+
     if output == 'json':
         return json.dumps(report)
     elif output == 'rst':
-        return generate_rst(report)
-    markup = generate_rst(report)
+        return generate_rst(report, dao.build.version)
+    markup = generate_rst(report, dao.build.version)
     return publish_string(markup, writer_name=output,
                           settings_overrides={'input_encoding': 'unicode'})
 
@@ -573,9 +172,8 @@ def server_static(filepath):
 
 
 @hook('before_request')
-def fetch_build():
-    global build
-    build = latest_build()
+def refresh_build():
+    dao.refresh_build()
 
 
 if __name__ == "__main__":
